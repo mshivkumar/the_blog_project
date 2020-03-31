537d1ef89d322de90b56afdd38e6735f3aa4dfa0
mshivkumar <mshiv317@gmail.com>
2020-03-31 18:34:46 +0530

    create article resources and CRUD operations

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,53 @@
+class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article sucessfully created"
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article sucessfully updated"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+end
